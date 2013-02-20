2898553aea3f630efee09cd610e0c3f06a10ccd8
Fiona Strain <fiona.strain1@gmail.com>
2013-02-19 21:48:15 -0500

    hi

--- db/migrate/20130214020611_create_teammates.rb
+++ db/migrate/20130214020611_create_teammates.rb
@@ -1,6 +1,5 @@
 class CreateTeammates < ActiveRecord::Migration
   def change
-    drop_table :teammates
     create_table :teammates do |t|
       t.string :name
       t.string :email
--- db/migrate/20130214020701_create_documents.rb
+++ db/migrate/20130214020701_create_documents.rb
@@ -1,6 +1,5 @@
 class CreateDocuments < ActiveRecord::Migration
   def change
-    drop_table :documents
     create_table :documents do |t|
       t.string :name
       t.datetime :datemodified
